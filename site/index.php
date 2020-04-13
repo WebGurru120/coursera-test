43fc86de5260549860f81e6e3f3f7935d4c98802
WebGurru120 <WebGurru123@gmail.com>
2020-04-13 19:06:05 +0500

    (Just changed the index.html to php)

---
+++ site/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Understanding GIt</title>
+</head>
+<body>
+		<h1>Understanding Git </h1>
+		<p> Finally i have understood Git a bit </p>
+
+</body>
+</html>
